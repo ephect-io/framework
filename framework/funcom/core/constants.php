45ccd889d6a9c5c76118cf6ea5b2a95060e1e61a
david-blanchard <dpjb@hotmail.fr>
2020-11-29 15:05:40 +0100

    No more global constant for now

--- framework/funcom/core/constants.php
+++ framework/funcom/core/constants.php
@@ -1,6 +1,2 @@
 <?php
 
-define('SITE_ROOT', dirname(dirname(dirname(__DIR__))) . DIRECTORY_SEPARATOR);
-define('SRC_ROOT', SITE_ROOT .  'src' . DIRECTORY_SEPARATOR);
-define('REL_CACHE_DIR', 'cache' . DIRECTORY_SEPARATOR);
-define('CACHE_DIR', SITE_ROOT . REL_CACHE_DIR);
