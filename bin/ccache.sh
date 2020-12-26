c86b4ddcba21083a140cb6bb903ad6d8929edf84
david-blanchard <dpjb@hotmail.fr>
2020-12-26 09:45:42 +0100

    Update tools

--- bin/ccache.sh
+++ bin/ccache.sh
@@ -1,5 +1,13 @@
 #!/bin/sh
-cd /Sites/funcom
+if [ -d /Sites/funcom ];
+then
+    cd /Site/funcom
+fi
+if [ -d /Users/david/Sites/CodePhoenixOrg/SDK/php/funcom ];
+then
+    cd /Users/david/Sites/CodePhoenixOrg/SDK/php/funcom
+fi
+
 sudo rm -rf cache
 echo Cache cleared
 echo
--- bin/chmod.sh
+++ bin/chmod.sh
@@ -1,5 +1,14 @@
 #!/bin/sh
-cd /Sites/funcom/framework
+if [ -d /Sites/funcom ];
+then
+    cd /Site/funcom
+fi
+if [ -d /Users/david/Sites/CodePhoenixOrg/SDK/php/funcom ];
+then
+    cd /Users/david/Sites/CodePhoenixOrg/SDK/php/funcom
+fi
+cd framework
+
 sudo find . -type d -exec chmod 775 {} \;
 sudo find . -type f -exec chmod 664 {} \;
 sudo chown -R vscode:vscode .
