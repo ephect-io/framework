cd1210249291cd77f98f30f089a11ab10adcf895
david-blanchard <dpjb@hotmail.fr>
2021-01-09 01:40:25 +0100

    Transform regex result

--- framework/funcom/components/generators/component_parser.php
+++ framework/funcom/components/generators/component_parser.php
@@ -7,11 +7,11 @@ use FunCom\Components\ComponentInterface;
 class ComponentParser
 {
 
-    protected  $html = '';
-    protected  $view = null;
-    protected  $useVariables = [];
-    protected  $parentHTML = '';
-    protected  $maker = null;
+    protected $html = '';
+    protected $view = null;
+    protected $useVariables = [];
+    protected $parentHTML = '';
+    protected $maker = null;
 
     public function __construct(ComponentInterface $view)
     {
@@ -40,27 +40,65 @@ class ComponentParser
         // $matches = $matches[0];
 
         $l = count($matches);
-        for($i = $l - 1; $i > -1; $i--) {
-            $match = $matches[$i];
+        for ($i = $l - 1; $i > -1; $i--) {
+            $matches[$i]['id'] = $i;
+            $matches[$i]['component'] = $matches[$i][0][0];
+            $matches[$i]['startsAt'] = $matches[$i][0][1];
+            $matches[$i]['props'] = $this->doArguments($matches[$i][0][0]);
+            // $matches[$i]['hasCloser'] = $matches[$i][1][0] === '/';
+            $matches[$i]['hasCloser'] = false;
 
-            if($match[1][0] === '/') {
+            if ($matches[$i][1][0] === '/') {
+                for ($j = $i - 1; $j > -1; $j--) {
+                    if ($matches[$i][2][0] === $matches[$j][2][0] && $matches[$j][1][0] === '') {
+                        $matches[$j]['closer'] = [
+                            'id' => $i,
+                            'parentId' => $j,
+                            'component' => $matches[$i][0][0],
+                            'startsAt' => $matches[$i][0][1],
+                        ];
+                        // $matches[$j]['hasCloser'] = true;
 
-                $needle = $match[2][0];
-                for($j = $i - 1; $j > -1; $j--) {
-                    $sibling = $matches[$j][2][0];
-                    $closing = $matches[$j][1][0] === '';
-
-                    if($needle === $sibling && $closing) {
-                        $matches[$j][3] = $match;
                         unset($matches[$i]);
-
-                    break;
+                        break;
                     }
                 }
             }
+
+            if (isset($matches[$i])) {
+                unset($matches[$i][0]);
+                unset($matches[$i][1]);
+                unset($matches[$i][2]);
+            }
         }
 
-        $result = $matches;
+        $list = [];
+        foreach ($matches as $key => $value) {
+            if(isset($value['closer'])) {
+                $value['hasCloser'] = true;
+            }
+            array_push($list, $value);
+        }
+
+        $result = $list;
+
+        return $result;
+    }
+
+    public function doArguments(string $componentArgs): ?array
+    {
+        $result = [];
+
+        $re = '/([A-Za-z0-9_]*)=("([\S\\\\\" ]*)"|\'([\S\\\\\' ]*)\'|\{([\S\\\\\{\}\(\)=\<\> ]*)\})/m';
+
+        preg_match_all($re, $componentArgs, $matches, PREG_SET_ORDER, 0);
+
+        foreach ($matches as $match) {
+            $key = $match[1];
+            $value = substr(substr($match[2], 1), 0, -1);
+
+            $result[$key] = $value;
+        }
 
         return $result;
     }
