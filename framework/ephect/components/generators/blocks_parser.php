19d68c96691f827c9a9c39aaca567c546e68616b
david-blanchard <dpjb@hotmail.fr>
2021-03-04 01:44:24 +0100

    Add a parser for the Blocks

---
+++ framework/ephect/components/generators/blocks_parser.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace Ephect\Components\Generators;
+
+use Ephect\Cache\Cache;
+use Ephect\Crypto\Crypto;
+use Ephect\IO\Utils;
+use Ephect\Registry\CacheRegistry;
+use Ephect\Registry\UseRegistry;
+use Ephect\Registry\ViewRegistry;
+
+class BlocksParser extends Parser
+{
+
+    public function doBlocks(): bool
+    {
+        $className = $this->view->getFullyQualifiedFunction();
+
+        $doc = new ComponentDocument($this->html);
+        $doc->matchAll();
+
+        $firstMatch = $doc->getNextMatch();
+        if ($firstMatch !== null && $firstMatch->hasCloser()) {
+
+            $functionName = $firstMatch->getName();
+            $parentClassName = UseRegistry::read($functionName);
+            $parentFilename = ViewRegistry::read($parentClassName);
+            $parentHtml = file_get_contents(SRC_ROOT . $parentFilename);
+
+            // $parentHtml = $this->doHtml($parentHtml);
+            // $chldHtml = $this->doHtml();
+            $token = '_' . str_replace('-', '', $this->view->getUID());
+
+            $parentDoc = new ComponentDocument($parentHtml);
+            $parentDoc->matchAll();
+
+            $parentHtml = $parentDoc->replaceMatches($doc, $this->html);
+
+            $parentHtml = str_replace($functionName, $functionName . $token, $parentHtml);
+
+            $childHtml = $this->html;
+
+            $childHtml = str_replace($functionName, $functionName . $token, $childHtml);
+
+            $functionFilename = strtolower($functionName);
+            $functionFilename = 'source_' . str_replace($functionFilename, $functionFilename . $token, $parentFilename);
+            if ($parentHtml !== '') {
+                $cacheFilename = Cache::getCacheFilename($functionFilename);
+                Utils::safeWrite($cacheFilename, $parentHtml);
+
+                $cacheFilename = CACHE_DIR . $this->view->getCachedSourceFilename();
+                Utils::safeWrite($cacheFilename, $childHtml);
+
+                // ViewRegistry::write($className, $cacheFilename);
+            }
+        }
+
+        if ($doc->getCount() > 0) {
+        }
+
+
+        return $this->html !== null;
+    }
+}
