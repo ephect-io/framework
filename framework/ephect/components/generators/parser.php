d55fb362aa69c602ed4dcacaf3880998281bdef8
david-blanchard <dpjb@hotmail.fr>
2021-04-10 18:33:55 +0000

    Don't mix heredoc with tags

--- framework/ephect/components/generators/parser.php
+++ framework/ephect/components/generators/parser.php
@@ -162,7 +162,7 @@ class Parser
         $useVars = array_values($this->useVariables);
         $use = count($useVars) > 0 ? 'use(' . implode(', ', $useVars) . ') ' : '';
 
-        $this->html = str_replace('(<<<HTML', 'function () ' . $use . '{?>', $this->html);
+        $this->html = str_replace('(<<< HTML', 'function () ' . $use . '{?>', $this->html);
         $this->html = str_replace('HTML);', "<?php\n\t};", $this->html);
 
         $result = $this->html !== '';
