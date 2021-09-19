2f4e0ef6319c9679617a3f618611109ef2fc0372
David Blanchard <david.blanchard71@icloud.com>
2021-09-19 18:25:23 +0200

    Fix scripts

--- bin/chmod.sh
+++ bin/chmod.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
 if [ ! -d ./cache ] || [ ! -d ./runtime ];
 then
-    echo "Be sure you are at the root of your Ephect site."
+    echo "Be sure you are at the root of your Ephect project."
     exit 1;
 fi
 
 sudo find . -type d -exec chmod 775 {} \;
 sudo find . -type f -exec chmod 664 {} \;
-sudo chown -R www-data:www-data .
+
+exit 0
