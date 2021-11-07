6f4c190e54412b74cd10bb5518568a9e2ee293a8
David Blanchard <dpjb@hotmail.fr>
2021-11-07 12:04:25 +0100

    Simplify egg app declaration

--- framework/apps/egg/src/app.php
+++ framework/apps/egg/src/app.php
@@ -2,25 +2,8 @@
 
 namespace Ephect\Apps\Egg;
 
-use Ephect\Apps\Egg\EggLib;
 use Ephect\CLI\Application;
 
 include 'includes.php';
-include 'lib.php';
 
-class Program extends Application
-{
-
-    public static function main($argv, $argc)
-    {
-        (new Program($argc, $argv));
-    }
-
-    public function __construct($argc, $argv)
-    {
-        parent::__construct($argv, $argc);
-    }
-
-}
-
-Program::main($argv, $argc);
+Application::create($argv, $argc);
