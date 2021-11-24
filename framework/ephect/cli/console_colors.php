78c8d1b1a5267d9dcea8201679ad532c2dc52bff
David Blanchard <dpjb@hotmail.fr>
2021-11-24 13:41:42 +0100

    Completely redesign ConsoleColors

--- framework/ephect/cli/console_colors.php
+++ framework/ephect/cli/console_colors.php
@@ -4,68 +4,51 @@ namespace Ephect\CLI;
 
 class ConsoleColors
 {
-    private $foreground_colors = [];
-    private $background_colors = [];
 
-    public function __construct()
-    {
-        // Set up shell colors
-        $this->foreground_colors['black'] = '0;30';
-        $this->foreground_colors['dark_gray'] = '1;30';
-        $this->foreground_colors['blue'] = '0;34';
-        $this->foreground_colors['light_blue'] = '1;34';
-        $this->foreground_colors['green'] = '0;32';
-        $this->foreground_colors['light_green'] = '1;32';
-        $this->foreground_colors['cyan'] = '0;36';
-        $this->foreground_colors['light_cyan'] = '1;36';
-        $this->foreground_colors['red'] = '0;31';
-        $this->foreground_colors['light_red'] = '1;31';
-        $this->foreground_colors['purple'] = '0;35';
-        $this->foreground_colors['light_purple'] = '1;35';
-        $this->foreground_colors['brown'] = '0;33';
-        $this->foreground_colors['yellow'] = '1;33';
-        $this->foreground_colors['light_gray'] = '0;37';
-        $this->foreground_colors['white'] = '1;37';
+    // Set up shell colors
+    const BLACK = '0;30';
+    const DARK_GRAY = '1;30';
+    const BLUE = '0;34';
+    const LIGHT_BLUE = '1;34';
+    const GREEN = '0;32';
+    const LIGHT_GREEN = '1;32';
+    const CYAN = '0;36';
+    const LIGHT_CYAN = '1;36';
+    const RED = '0;31';
+    const LIGHT_RED = '1;31';
+    const PURPLE = '0;35';
+    const LIGHT_PURPLE = '1;35';
+    const BROWN = '0;33';
+    const YELLOW = '1;33';
+    const LIGHT_GRAY = '0;37';
+    const WHITE = '1;37';
 
-        $this->background_colors['black'] = '40';
-        $this->background_colors['red'] = '41';
-        $this->background_colors['green'] = '42';
-        $this->background_colors['yellow'] = '43';
-        $this->background_colors['blue'] = '44';
-        $this->background_colors['magenta'] = '45';
-        $this->background_colors['cyan'] = '46';
-        $this->background_colors['light_gray'] = '47';
-    }
+    const BACKGROUND_BLACK = '40';
+    const BACKGROUND_RED = '41';
+    const BACKGROUND_GREEN = '42';
+    const BACKGROUND_YELLOW = '43';
+    const BACKGROUND_BLUE = '44';
+    const BACKGROUND_MAGENTA = '45';
+    const BACKGROUND_CYAN = '46';
+    const BACKGROUND_LIGHT_GRAY = '47';
 
-    // Returns colored string
-    public function getColoredString($string, $foreground_color = null, $background_color = null): string
+    // Returns coloRED string
+    public static function getColoredString($string, $foreground_color = null, $background_color = null): string
     {
         $colored_string = "";
 
         // Check if given foreground color found
-        if (isset($this->foreground_colors[$foreground_color])) {
-            $colored_string .= "\033[" . $this->foreground_colors[$foreground_color] . "m";
+        if (isset($foreground_color)) {
+            $colored_string .= "\033[" . $foreground_color . "m";
         }
         // Check if given background color found
-        if (isset($this->background_colors[$background_color])) {
-            $colored_string .= "\033[" . $this->background_colors[$background_color] . "m";
+        if (isset($background_color)) {
+            $colored_string .= "\033[" . $background_color . "m";
         }
 
         // Add string and end coloring
         $colored_string .=  $string . "\033[0m";
 
         return $colored_string;
-    }
-
-    // Returns all foreground color names
-    public function getForegroundColors(): array
-    {
-        return array_keys($this->foreground_colors);
-    }
-
-    // Returns all background color names
-    public function getBackgroundColors(): array
-    {
-        return array_keys($this->background_colors);
     }
 }
