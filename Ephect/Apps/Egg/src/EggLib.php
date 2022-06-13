b388873535605da72fa3704855b175deb550c9f5
David Blanchard <david.blanchard71@icloud.com>
2022-06-13 06:18:21 +0200

    Delete useless commands

--- Ephect/Apps/Egg/src/EggLib.php
+++ Ephect/Apps/Egg/src/EggLib.php
@@ -13,86 +13,12 @@ class EggLib extends Element
 {
 
     /**
-     * Defines the full directory tree of a Ephect web application
-     */
-    private $directories = [
-        'app',
-        'app/business',
-        'app/controllers',
-        'app/models',
-        'app/rest',
-        'app/scripts',
-        'app/templates',
-        'app/views',
-        'app/webservices',
-        'cache',
-        'cert',
-        'config',
-        'css',
-        'data',
-        'docker',
-        'fonts',
-        'logs',
-        'media',
-        'media/images',
-        'runtime',
-        'themes',
-        'tmp',
-        'tools',
-        'web',
-        'web/css',
-        'web/css/images',
-        'web/fonts',
-        'web/js',
-        'web/js/runtime',
-        'web/media',
-        'web/media/images',
-    ];
-
-    /**
      * Constructor
      */
     public function __construct(Application $parent)
     {
         parent::__construct($parent);
 
-    }
-
-    /**
-     * Create the skeleton of the application
-     */
-    public function createTree()
-    {
-        $this->parent->writeLine("Current directory %s", APP_CWD);
-
-        sort($this->directories);
-        foreach ($this->directories as $directory) {
-            if (!file_exists($directory)) {
-                $this->parent->writeLine("Creating directory %s", $directory);
-                mkdir($directory, 0755, true);
-            } else {
-                $this->parent->writeLine("Directory %s already exist", $directory);
-            }
-        }
-    }
-
-    /**
-     * Deletes recursively all known directories of the application
-     */
-    public function deleteTree()
-    {
-        $this->parent->writeLine("Current directory %s", APP_CWD);
-
-        rsort($this->directories);
-        foreach ($this->directories as $directory) {
-            $dir = APP_CWD . $directory;
-            if (file_exists($dir)) {
-                $this->parent->writeLine("Removing directory %s", $dir);
-                Utils::safeRmdir($dir);
-            } else {
-                $this->parent->writeLine("Cannot find directory %s", $dir);
-            }
-        }
     }
 
     public function createQuickstart(): void
