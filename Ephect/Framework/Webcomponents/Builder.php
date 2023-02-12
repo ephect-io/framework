895efc4faa46e56dd019cd71f7e40d296b703d36
David Blanchard <dpjb@hotmail.fr>
2023-02-12 16:03:51 +0100

    Add entrypoint to builder

--- Ephect/Framework/Webcomponents/Builder.php
+++ Ephect/Framework/Webcomponents/Builder.php
@@ -4,5 +4,15 @@ namespace Ephect\Framework\Webcomponents;
 
 class Builder
 {
+  /**
+     * Register all components of the application
+     *
+     * @return void
+     */
+    public function perform(): void
+    {
+   
 
+  
+    }
 }
