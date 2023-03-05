e716dd8e781237767b1ccd15f4348f670c51100d
David Blanchard <david.blanchard71@icloud.com>
2023-03-05 12:31:46 +0100

    Refactor ComponentStructure.php

--- Ephect/Framework/Components/ComponentStructure.php
+++ Ephect/Framework/Components/ComponentStructure.php
@@ -6,21 +6,21 @@ use Ephect\Framework\Core\Structure;
 
 class ComponentStructure extends Structure
 {
-    public $uid;
-    public $motherUID;
-    public $id;
-    public $class;
-    public $component;
-    public $name;
-    public $method;
-    public $text;
-    public $parentId;
-    public $depth;
-    public $startsAt;
-    public $endsAt;
-    public $isSibling;
-    public $hasCloser;
-    public $closer;
-    public $props;
-    public $node;
+    public string $uid = '';
+    public string $motherUID = '';
+    public string $id = '';
+    public ?string $class = '';
+    public string $component = '';
+    public string $name = '';
+    public string $method = '';
+    public string $text = '';
+    public string $parentId = '';
+    public int $depth = 0;
+    public int $startsAt = 0;
+    public int $endsAt = 0;
+    public ?bool $isSibling = false;
+    public bool $hasCloser = false;
+    public ?array $closer = [];
+    public array $props = [];
+    public bool|array|null $node = null;
 }
