877df78532762152e55cadfa5029e80752b05e7e
David Blanchard <dpjb@hotmail.fr>
2023-04-03 02:00:16 +0200

    Backup OpenComponentParser

--- Ephect/Framework/Components/Generators/TokenParsers/OpenComponentsParser.php
+++ Ephect/Framework/Components/Generators/TokenParsers/OpenComponentsParser.php
@@ -6,6 +6,8 @@ use Ephect\Framework\CLI\Console;
 use Ephect\Framework\Components\ComponentEntityInterface;
 use Ephect\Framework\IO\Utils;
 use Ephect\Framework\Registry\ComponentRegistry;
+use Ephect\Framework\Registry\WebComponentRegistry;
+use Ephect\Framework\WebComponents\ManifestReader;
 
 final class OpenComponentsParser extends AbstractTokenParser
 {
@@ -13,23 +15,26 @@ final class OpenComponentsParser extends AbstractTokenParser
     {
         $this->result = [];
         $this->useVariables = $parameter;
-     
+
         $comp = $this->component;
         $comp->resetDeclaration();
         $decl = $comp->getDeclaration();
         $cmpz = $decl->getComposition();
 
-        if($cmpz === null || !$cmpz->hasCloser()) {
+        if ($cmpz === null || !$cmpz->hasCloser()) {
             return;
         }
+
+        // $parentProps = $cmpz->props();
 
         $subject = $this->html;
 
         $closure = function (ComponentEntityInterface $item, int $index)  use (&$subject, &$result) {
 
-            if(!$item->hasCloser()) {
+            if (!$item->hasCloser()) {
                 return;
             }
+            $uid = $item->getUID();
 
             $opener = $item->getText();
             $closer = ((object) $item->getCloser())->text;
@@ -38,46 +43,63 @@ final class OpenComponentsParser extends AbstractTokenParser
             $componentBody = $item->getInnerHTML();
             $componentArgs = $this->useVariables;
             $componentArgs = $item->props() !== null ? array_merge($componentArgs, $item->props()) : $componentArgs;
-            
-            if($componentName === 'FakeFragment') {
+
+            if ($componentName === 'FakeFragment') {
                 return;
             }
 
-            if($componentName === 'Fragment') {
+            if ($componentName === 'Fragment') {
                 return;
             }
 
-            if($componentName === 'Slot') {
+            if ($componentName === 'Slot') {
                 return;
             }
 
             $motherUID = $this->component->getMotherUID();
             $decl = $this->component->getDeclaration();
-    
+
             $propsArgs = $componentArgs === null ?: self::doArgumentsToString($componentArgs);
             $props = (($propsArgs === null) ? "[]" : $propsArgs);
 
             $propsKeys = $this->argumentsKeys($this->useVariables);
-            
+
             $useChildren = $decl->hasArguments() || count($propsKeys) ? $this->useArguments($propsKeys) : ' ';
 
             $className = $this->component->getFullyQualifiedFunction() ?: $componentName;
             $name = $this->component->getFunction() ?: $componentName;
-            $classArgs = '[]';
-    
-            $fqComponentName = '\\' . ComponentRegistry::read($componentName);
-    
+            $classArgs = '[]'; //'json_decode("' . json_encode($parentProps) . '")';
+
+            $fqComponentName = ComponentRegistry::read($componentName);
+            $filename = ComponentRegistry::read($fqComponentName);
+
+            $wopener = '';
+            $wcloser = '';
+            if ($filename === null) {
+                $filename = WebComponentRegistry::read($fqComponentName);
+                if ($filename !== null) {
+                    // $uid = WebComponentRegistry::read($filename);
+                    $reader = new ManifestReader($motherUID, $componentName);
+                    $manifest = $reader->read();
+                    $tag = $manifest->getTag();
+                    $text = str_replace($componentName, $tag, $opener . $componentBody . $closer);
+                    // $wopener = str_replace($componentName, $tag, $opener);
+                    // $wcloser = str_replace($componentName, $tag, $closer);
+                    Utils::safeWrite(CACHE_DIR . $this->component->getMotherUID() . DIRECTORY_SEPARATOR . $componentName . $uid . '.txt', $text);
+                }
+            }
+
             $preComponentBody = '';
-            if(count($propsKeys) === 1 && $propsKeys[0] === "\$children") {
+            if (count($propsKeys) === 1 && $propsKeys[0] === "\$children") {
                 $preComponentBody .= "\t\t\t<?php \$props = \$children->props(); ?>\n";
                 $preComponentBody .= "\t\t\t<?php foreach(\$props as \$key => \$value) { ?>\n";
                 $preComponentBody .= "\t\t\t<?php     $\$key = \"\$value\"; ?>\n";
                 $preComponentBody .= "\t\t\t<?php } ?>\n";
             }
-            
-            $componentRender = "<?php \$struct = new \\Ephect\\Framework\\Components\\ChildrenStructure(['props' => (object) $props, 'buffer' => function()$useChildren{?>\n\n$preComponentBody$componentBody\n<?php\n}, 'class' => '$className', 'name' => '$name', 'parentProps' => $classArgs, 'motherUID' => '$motherUID']); ?>\n";
+
+            $componentRender = "<?php \$struct = new \\Ephect\\Framework\\Components\\ChildrenStructure(['props' => (object) $props, 'buffer' => function()$useChildren{?>\n\n$preComponentBody$wopener$componentBody$wcloser\n<?php\n}, 'uid' => '$uid', 'motherUID' => '$motherUID', 'class' => '$className', 'name' => '$name', 'parentProps' => $classArgs]); ?>\n";
             $componentRender .= "\t\t\t<?php \$children = new \\Ephect\\Framework\\Components\\Children(\$struct); ?>\n";
-            $componentRender .= "\t\t\t<?php \$fn = $fqComponentName(\$children); \$fn(); ?>\n";
+            $componentRender .= "\t\t\t<?php \$fn = \\$fqComponentName(\$children); \$fn(); ?>\n";
 
             $subject = str_replace($componentBody, $componentRender, $subject);
 
@@ -94,17 +116,16 @@ final class OpenComponentsParser extends AbstractTokenParser
             Utils::safeWrite(CACHE_DIR . $this->component->getMotherUID() . DIRECTORY_SEPARATOR . $filename, $subject);
 
             $this->result[] = $componentName;
-
         };
 
         $closure($cmpz, 0);
-        if($cmpz->hasChildren()) {
+        if ($cmpz->hasChildren()) {
             $cmpz->forEach($closure, $cmpz);
-        } 
+        }
 
         $this->html = $subject;
     }
-    
+
     private function argumentsKeys(array $componentArgs): ?array
     {
         $result = [];
@@ -121,12 +142,11 @@ final class OpenComponentsParser extends AbstractTokenParser
         $result = '';
 
         $args = implode(', ', $argumentsKeys);
-        if($args === '') {
+        if ($args === '') {
             return ' ';
         }
 
         return " use (" . $args . ")";
-
     }
 
     public static function doArgumentsToString(array $componentArgs): ?string
@@ -138,9 +158,8 @@ final class OpenComponentsParser extends AbstractTokenParser
                 $value = json_encode($value);
             }
             $pair = '"' . $key . '" => "' . urlencode($value) . '", ';
-            if($value[0] === '$') {
+            if ($value[0] === '$') {
                 $pair = '"' . $key . '" => ' . $value . ', ';
-
             }
             $result .= $pair;
         }
