a23a09bdcdc246f4c325000881d0da925a6c79b6
David Blanchard <dpjb@hotmail.fr>
2023-04-10 22:48:01 +0200

    Add HtmlSaver plugin to store html result on file

---
+++ Ephect/Plugins/HtmlSaver/HtmlSaverServiceInterface.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Ephect\Plugins\HtmlSaver;
+
+interface HtmlSaveServiceInterface
+{
+    public function canRender(): bool;
+    public function isPending(): bool;
+    public function markAsPending(): void;
+    public function storeHTML(string $html): void;
+
+}
