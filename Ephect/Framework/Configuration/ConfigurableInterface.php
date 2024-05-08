ee4cb330faa732df29db14a9c3a0a95f3e9c4645
David Blanchard <dpjb@hotmail.fr>
2024-05-08 17:24:54 +0200

    Add configuration management files.

---
+++ Ephect/Framework/Configuration/ConfigurableInterface.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace Ephect\Framework\Configuration;
+
+interface ConfigurableInterface {
+    public function configure() : void;
+    public function loadConfiguration(string $filename) : bool;
+}
