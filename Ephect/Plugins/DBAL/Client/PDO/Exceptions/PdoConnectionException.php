48f979024a9faeae44c630c24988d9993e61a24b
David Blanchard <dpjb@hotmail.fr>
2024-05-08 22:41:00 +0200

    Update DBAL plugin files

---
+++ Ephect/Plugins/DBAL/Client/PDO/Exceptions/PdoConnectionException.php
@@ -0,0 +1,11 @@
+<?php
+namespace Ephect\Plugins\DBAL\CLient\PDO\Exceptions;
+use Exception;
+
+class PdoConnectionException extends Exception
+{
+    public function __construct(string $message = "" , int $code = 0, \Throwable $previous = NULL)
+    {
+        parent::__construct($message, $code, $previous);
+    }
+}
