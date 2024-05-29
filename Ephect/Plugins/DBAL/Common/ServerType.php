1d8387a1f2b78d75cccb10763570a2d706592cf6
David Blanchard <dpjb@hotmail.fr>
2024-05-29 22:35:10 +0200

    ServerTypeEnum is not relevant by now.

---
+++ Ephect/Plugins/DBAL/Common/ServerType.php
@@ -0,0 +1,10 @@
+<?php
+namespace Ephect\Plugins\DBAL;
+
+class ServerType
+{
+    public const MYSQL = 'mysql';
+    public const SQLSERVER = 'sqlsrv';
+    public const SQLITE = 'sqlite';
+    public const PGSQL = 'pgsql';
+}
