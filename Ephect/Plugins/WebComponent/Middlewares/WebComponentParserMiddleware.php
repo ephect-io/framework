79fb19d8d410498b6833917edd8ee7b886ec51d7
David Blanchard <dpjb@hotmail.fr>
2024-07-24 18:31:13 +0200

    WebComponent plugin can now be retreived in the State registry

--- Ephect/Plugins/WebComponent/Middlewares/WebComponentParserMiddleware.php
+++ Ephect/Plugins/WebComponent/Middlewares/WebComponentParserMiddleware.php
@@ -3,14 +3,16 @@
 namespace Ephect\Plugins\WebComponent\Middlewares;
 
 use Ephect\Framework\Components\ComponentEntityInterface;
+use Ephect\Framework\Middlewares\ComponentParserMiddlewareInterface;
 use Ephect\Framework\Registry\StateRegistry;
 use function Ephect\Hooks\useState;
 
-class WebComponentParserMiddleware
+class WebComponentParserMiddleware implements ComponentParserMiddlewareInterface
 {
     public function parse(ComponentEntityInterface|null $parent, string $motherUID, string $funcName, string $props, array $arguments): void
     {
+        StateRegistry::load();
         useState(["middlewares" => [WebComponentBuilderMiddleware::class => (object) $arguments],]);
-        StateRegistry::saveByMotherUid($motherUID, true);
+        StateRegistry::saveByMotherUid($motherUID);
     }
 }
