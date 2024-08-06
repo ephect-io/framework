43b2e93e8cc9c88360b117d44ab9a23470dd3bbd
David Blanchard <dpjb@hotmail.fr>
2024-08-06 17:08:26 +0200

    Add fucntions to read and write plugin paths.

--- Ephect/Framework/Registry/PluginRegistry.php
+++ Ephect/Framework/Registry/PluginRegistry.php
@@ -2,6 +2,9 @@
 
 namespace Ephect\Framework\Registry;
 
+use Ephect\Framework\Utils\File;
+use Ephect\Framework\Utils\Text;
+
 class PluginRegistry extends AbstractStaticRegistry
 {
     private static ?RegistryInterface $instance = null;
@@ -12,12 +15,38 @@ class PluginRegistry extends AbstractStaticRegistry
         unlink(self::$instance->getCacheFilename());
     }
 
-    public static function getInstance(): RegistryInterface
+    public static function getInstance(): AbstractStaticRegistry
     {
         if (self::$instance === null) {
             self::$instance = new PluginRegistry;
         }
 
         return self::$instance;
+    }
+
+    public static function readPluginPaths(): array
+    {
+        $configDir = applicationConfigPath();
+
+        $filename = $configDir . "pluginsPaths.php";
+
+        $paths = [];
+        if(is_file($filename)) {
+            $paths = require $filename;
+        }
+
+        return [$filename, $paths];
+    }
+
+    public static function savePluginPaths(array $paths): void
+    {
+        $configDir = applicationConfigPath();
+
+        $filename = $configDir . "pluginsPaths.php";
+
+        $json = json_encode($paths);
+        $pluginsPaths = Text::jsonToPhpReturnedArray($json, true);
+
+        File::safeWrite($filename, $pluginsPaths);
     }
 }
