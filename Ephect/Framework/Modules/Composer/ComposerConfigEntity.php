8c8446ad854705a28efb75ab85e3dafd3c7d74b9
David Blanchard <dpjb@hotmail.fr>
2024-08-17 17:13:02 +0200

    Add some properties to ComposerConfig Entity/Structure.

--- Ephect/Framework/Modules/Composer/ComposerConfigEntity.php
+++ Ephect/Framework/Modules/Composer/ComposerConfigEntity.php
@@ -2,12 +2,29 @@
 
 namespace Ephect\Framework\Modules\Composer;
 
-use Ephect\Framework\Entity\Entity;
 use Ephect\Framework\Manifest\ManifestEntity;
 
 class ComposerConfigEntity extends ManifestEntity
 {
+
+    private string $name;
+
+    private string $type;
+
+    private string $homepage;
+
+    private string $license;
+
+    private string $description;
+
+    private array $authors;
+
+    private array $autoload;
+
+    private string $minimumStability;
+
     private array $require = [];
+
     private array $requireDev = [];
 
     public function __construct(?ComposerConfigStructure $structure = null)
@@ -19,6 +36,46 @@ class ComposerConfigEntity extends ManifestEntity
         if($structure instanceof ComposerConfigStructure) {
             $this->bindStructure();
         }
+    }
+
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    public function getType(): string
+    {
+        return $this->type;
+    }
+
+    public function getHomepage(): string
+    {
+        return $this->homepage;
+    }
+
+    public function getLicense(): string
+    {
+        return $this->license;
+    }
+
+    public function getDescription(): string
+    {
+        return $this->description;
+    }
+
+    public function getAuthors(): array
+    {
+        return $this->authors;
+    }
+
+    public function getAutoload(): array
+    {
+        return $this->autoload;
+    }
+
+    public function getMinimumStability(): string
+    {
+        return $this->minimumStability;
     }
 
     public function getRequire(): array
@@ -33,6 +90,14 @@ class ComposerConfigEntity extends ManifestEntity
 
     private function bindStructure()
     {
+        $this->name = $this->structure->name;
+        $this->type = $this->structure->type;
+        $this->homepage = $this->structure->homepage;
+        $this->license = $this->structure->license;
+        $this->description = $this->structure->description;
+        $this->authors = $this->structure->authors;
+        $this->autoload = $this->structure->autoload;
+        $this->minimumStability = $this->structure->minimumStability;
         $this->require = $this->structure->require;
         $this->requireDev = $this->structure->requireDev;
     }
--- Ephect/Framework/Modules/Composer/ComposerConfigStructure.php
+++ Ephect/Framework/Modules/Composer/ComposerConfigStructure.php
@@ -7,6 +7,23 @@ use Ephect\Framework\Structure\Structure;
 
 class ComposerConfigStructure extends Structure
 {
+    public string $name;
+
+    public string $type;
+
+    public string $homepage;
+
+    public string $license;
+
+    public string $description;
+
+    public array $authors;
+
+    public array $autoload;
+
+    #[JsonProperty(name: "minimum-stability")]
+    public string $minimumStability;
+
     public array $require = [];
 
     #[JsonProperty(name: "require-dev")]
