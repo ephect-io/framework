464a3802c71fefb7533f7f72b5858ebb6191fdc1
dpjb <dpjb@hotmail.fr>
2024-08-21 21:32:21 +0200

    Move ApplicationIgniter from WebApp to Framework.

---
+++ Ephect/Framework/Web/ApplicationIgniter.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Ephect\Framework\Web;
+
+use Ephect\Framework\Middlewares\ApplicationStateMiddlewareInterface;
+use Ephect\Framework\Registry\FrameworkRegistry;
+use function Ephect\Hooks\useState;
+
+class ApplicationIgniter
+{
+    public function ignite(): void
+    {
+
+        [$state] = useState();
+
+        if ($state === null || !isset($state['middlewares'])) {
+            return;
+        }
+
+        $middlewares = (object)$state['middlewares'];
+        foreach ($middlewares as $className => $arguments) {
+            $filename = FrameworkRegistry::read($className);
+            if (is_file($filename)) {
+                if (is_subclass_of($className, ApplicationStateMiddlewareInterface::class)) {
+                    include_once $filename;
+                    $middleware = new $className;
+                    $middleware->ignite((object)$arguments);
+                }
+            }
+
+        }
+    }
+}
