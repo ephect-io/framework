e18c304f515c7c9dddedae327e76d61569fb000c
dpjb <dpjb@hotmail.fr>
2024-09-05 14:50:34 +0200

    Add WebComponents module

---
+++ Ephect/Modules/WebComponent/Manifest/ManifestReader.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Ephect\Modules\WebComponent\Manifest;
+
+use Ephect\Framework\Utils\File;
+use Ephect\Modules\WebComponent\Common;
+
+class ManifestReader
+{
+    public function __construct(private readonly string $motherUID, private readonly string $name)
+    {
+    }
+
+    public function read(): ManifestEntity
+    {
+
+        $manifestFilename = 'manifest.json';
+        $manifestCache = CACHE_DIR . $this->motherUID . DIRECTORY_SEPARATOR . $this->name . '.' . $manifestFilename;
+
+        if (!file_exists($manifestCache)) {
+            copy(Common::getCustomWebComponentRoot() . $this->name . DIRECTORY_SEPARATOR . $this->name . '.' . $manifestFilename, $manifestCache);
+        }
+
+        $manifestJson = File::safeRead($manifestCache);
+        $manifest = json_decode($manifestJson, JSON_OBJECT_AS_ARRAY);
+
+        $struct = new ManifestStructure($manifest);
+
+        return new ManifestEntity($struct);
+    }
+}
---
+++ Ephect/Modules/WebComponent/WebComponent.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Ephect\Modules\WebComponent;
+
+use Ephect\Forms\Components\Application\ApplicationComponent;
+use Ephect\Forms\Registry\ComponentRegistry;
+use Ephect\Framework\Templates\TemplateMaker;
+
+class WebComponent extends ApplicationComponent
+{
+
+    public function makeComponent(string $filename, string &$html): void
+    {
+        $info = (object) pathinfo($filename);
+        $namespace = CONFIG_NAMESPACE;
+        $function = $info->filename;
+
+        $componentTextMaker =  new TemplateMaker(MODULE_SRC_DIR . 'Templates' . DIRECTORY_SEPARATOR . 'Component.tpl');
+        $componentTextMaker->make(['funcNamespace' => $namespace, 'funcName' => $function, 'funcBody' => '', 'html' => $html]);
+        $componentTextMaker->save(COPY_DIR . $filename);
+
+    }
+
+    public function analyse(): void
+    {
+        parent::analyse();
+
+        ComponentRegistry::write($this->getFullyQualifiedFunction(), $this->getSourceFilename());
+        ComponentRegistry::safeWrite($this->getFunction(), $this->getFullyQualifiedFunction());
+        ComponentRegistry::save();
+    }
+
+    public function parse(): void
+    {
+        parent::parse();
+        $this->cacheHtml();
+    }
+
+}
