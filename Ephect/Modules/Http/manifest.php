d598a20eda75a6a67246ee10cca380ad8b08f226
dpjb <dpjb@hotmail.fr>
2024-09-07 12:11:40 +0200

    Add Http manifest.

---
+++ Ephect/Modules/Http/manifest.php
@@ -0,0 +1,9 @@
+<?php
+return [
+    "tag" => "",
+    "name" => "ephect-io/response-header",
+    "entrypoint" => \Ephect\Forms\Components\Plugin::class,
+    "templates" => "!",
+    "description" => "Ephect framework component that sends custom response headers.",
+    "version" => "0.7.2",
+];
