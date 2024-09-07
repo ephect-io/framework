9823f9451a3aabd118ccee0b70356f1ce29e5556
dpjb <dpjb@hotmail.fr>
2024-09-07 12:05:16 +0200

    Add DataAccess manifest.

---
+++ Ephect/Modules/DataAccess/manifest.php
@@ -0,0 +1,9 @@
+<?php
+return [
+    "tag" => "DbalConnection",
+    "name" => "ephect-io/data-access",
+    "entrypoint" => null,
+    "templates" => "!",
+    "description" => "Ephect framework database access layer.",
+    "version" => "0.7.2",
+];
