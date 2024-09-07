fc65afcfa9fcf4876bbcbdbdd109a585d578b028
dpjb <dpjb@hotmail.fr>
2024-09-07 13:00:36 +0200

    Add Routing manifest.

---
+++ Ephect/Modules/Routing/manifest.php
@@ -0,0 +1,9 @@
+<?php
+return [
+    "tag" => "",
+    "name" => "ephect-io/router",
+    "entrypoint" => \Ephect\Forms\Components\Plugin::class,
+    "templates" => "!",
+    "description" => "Ephect framework module that manages routes like components.",
+    "version" => "0.7.2",
+];
