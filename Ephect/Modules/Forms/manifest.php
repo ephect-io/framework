8f87ab900f269b31f40746d70205a8c0078f012e
dpjb <dpjb@hotmail.fr>
2024-09-07 12:07:25 +0200

    Add Forms manifest.

---
+++ Ephect/Modules/Forms/manifest.php
@@ -0,0 +1,9 @@
+<?php
+return [
+    "tag" => "",
+    "name" => "ephect-io/forms",
+    "entrypoint" => null,
+    "templates" => "!",
+    "description" => "Ephect framework components oriented templating system.",
+    "version" => "0.7.2",
+];
