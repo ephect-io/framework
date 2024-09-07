17919ae8af5091aadf403da8c1d655ce20fc316c
dpjb <dpjb@hotmail.fr>
2024-09-07 13:16:18 +0200

    Add WebComponent manifest.

---
+++ Ephect/Modules/WebComponent/manifest.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+    "tag" => "WebComponent",
+    "name" => "ephect-io/web-component",
+    "entrypoint" => \Ephect\Modules\WebComponent\WebComponent::class,
+    "templates" => "WebComponents",
+    "description" => "An Ephect framework module to build web components.",
+    "version" => "1.0.0",
+];
