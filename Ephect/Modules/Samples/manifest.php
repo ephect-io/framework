2f9a9baac19d4e0e88b6f89294ead507bf6621a8
dpjb <dpjb@hotmail.fr>
2024-09-07 13:02:18 +0200

    Add Samples manifest.

---
+++ Ephect/Modules/Samples/manifest.php
@@ -0,0 +1,9 @@
+<?php
+return [
+    "tag" => "",
+    "name" => "ephect-io/samples",
+    "entrypoint" => null,
+    "templates" => "!",
+    "description" => "Ephect framework QuickStart and Skeleton sample applications.",
+    "version" => "0.7.2",
+];
