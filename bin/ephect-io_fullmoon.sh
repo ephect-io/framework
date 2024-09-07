0ae19bac82b0e4cd752cea98f3c40bacbada3e97
dpjb <dpjb@hotmail.fr>
2024-09-07 17:04:14 +0200

    Add a script to install modules.

---
+++ bin/ephect-io_fullmoon.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env sh
+MODULES_PATH=vendor/ephect-io/fullmoon/Ephect/Modules
+LIST=$(ls -A $MODULES_PATH)
+
+#find $MODULES_PATH -maxdepth 1 -type d -exec php use install:module "{}" $1 $2 \;
+for i in $LIST;
+do
+   echo "php use install:module '$MODULES_PATH/$i' $1 $2"
+done;
+
