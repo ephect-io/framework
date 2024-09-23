40a6ae13ddc66a186dfadd8cf42071df6780b1c0
dpjb <dpjb@hotmail.fr>
2024-09-23 23:38:15 +0200

    Unique copies are now really unique.

--- Ephect/Modules/WebApp/Builder/Copiers/Strategy/CopyAsUniqueStrategy.php
+++ Ephect/Modules/WebApp/Builder/Copiers/Strategy/CopyAsUniqueStrategy.php
@@ -2,12 +2,16 @@
 
 namespace Ephect\Modules\WebApp\Builder\Copiers\Strategy;
 
+use Ephect\Framework\ElementUtils;
 use Ephect\Framework\Utils\File;
+use Ephect\Modules\Forms\Components\Component;
 
 class CopyAsUniqueStrategy implements CopierStrategyInterface
 {
+    private string $uniqueDomain = '';
     public function __construct()
     {
+        $this->uniqueDomain = "Unique" . uniqid();
         File::safeMkDir(UNIQUE_DIR);
     }
 
@@ -24,6 +28,23 @@ class CopyAsUniqueStrategy implements CopierStrategyInterface
         File::safeMkDir(UNIQUE_DIR . $root . $dirname);
         $contents = file_get_contents($path . $dirname . $basename);
 
-        copy($path . $dirname . $basename, UNIQUE_DIR . $root . $dirname . $basename);
+        [
+            $namespace,
+            $functionName,
+            $parameters,
+            $returnedType,
+            $startsAt
+        ] = ElementUtils::getFunctionDefinition($contents);
+
+        if ($startsAt == -1) {
+            $comp = Component::createByHtml($contents);
+            $comp->makeComponent($filename, $contents);
+        }
+
+        $contents = str_replace('namespace '. CONFIG_NAMESPACE, 'namespace ' .  $this->uniqueDomain, $contents);
+        $contents = preg_replace('/^use ' . CONFIG_NAMESPACE . '/m', 'use ' .  $this->uniqueDomain, $contents);
+        $contents = preg_replace('/^use function ' . CONFIG_NAMESPACE . '/m', 'use function ' .  $this->uniqueDomain, $contents);
+
+        File::safeWrite(UNIQUE_DIR . $root . $dirname . $basename, $contents);
     }
 }
