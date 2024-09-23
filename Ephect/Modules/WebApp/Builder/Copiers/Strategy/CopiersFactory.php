b2ca570941d4de536500310f96273228c7d6ffc0
dpjb <dpjb@hotmail.fr>
2024-09-23 23:38:15 +0200

    Unique copies are now really unique.

--- Ephect/Modules/WebApp/Builder/Copiers/Strategy/CopiersFactory.php
+++ Ephect/Modules/WebApp/Builder/Copiers/Strategy/CopiersFactory.php
@@ -7,8 +7,8 @@ class CopiersFactory
     public static function createCopier(bool $asUnique = false): CopierStrategyInterface
     {
         return match ($asUnique) {
-            true => new CopyAsUniqueStrategy,
-            false => new CopyAsIsStrategy,
+            true => new CopyAsUniqueStrategy(),
+            false => new CopyAsIsStrategy(),
         };
     }
 }
