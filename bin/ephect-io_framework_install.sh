57b2514a1df3c6cbeed92c5cea196af86e571363
dpjb <dpjb@hotmail.fr>
2025-03-25 15:20:06 +0100

    Fix install scripts.

--- bin/ephect-io_framework_install.sh
+++ bin/ephect-io_framework_install.sh
@@ -1,5 +1,11 @@
 #!/usr/bin/env sh
-MODULES_PATH=vendor/ephect-io/fullmoon/Ephect/Modules
+
+FILE_PATH=$(realpath -s $0);
+CWD=$(dirname $FILE_PATH);
+PARENT_DIR=$(dirname $CWD)
+FRAMEWORK_DIR=$(head $PARENT_DIR/config/framework)
+
+MODULES_PATH=$FRAMEWORK_DIR/Modules
 LIST=$(ls -A $MODULES_PATH)
 
 #find $MODULES_PATH -maxdepth 1 -type d -exec php use install:module "{}" $1 $2 \;
