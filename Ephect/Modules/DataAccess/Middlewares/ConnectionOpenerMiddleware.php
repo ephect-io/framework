00d37a3ade0774f3dcc5647b796d90c3d7ea1d19
David Blanchard <dpjb@hotmail.fr>
2025-09-21 02:24:37 +0200

    Can now connect on first access, but need to use event.

--- Ephect/Modules/DataAccess/Middlewares/ConnectionOpenerMiddleware.php
+++ Ephect/Modules/DataAccess/Middlewares/ConnectionOpenerMiddleware.php
@@ -2,17 +2,19 @@
 
 namespace Ephect\Modules\DataAccess\Middlewares;
 
+use Ephect\Framework\Logger\Logger;
 use Ephect\Framework\Middlewares\ApplicationStateMiddlewareInterface;
 use Ephect\Modules\DataAccess\Client\PDO\PdoConnection;
+use function Ephect\Hooks\useMemory;
 use function Ephect\Hooks\useState;
 
 class ConnectionOpenerMiddleware implements ApplicationStateMiddlewareInterface
 {
 
-    public function __invoke(object $arguments)
+    public function __invoke(object $arguments): void
     {
         $conn = PdoConnection::opener($arguments->conf);
-        useState(["$arguments->conf" => $conn,]);
+        useMemory(["$arguments->conf" => $conn,]);
         // Dispatch event
     }
 }
--- Ephect/Modules/DataAccess/Middlewares/ConnectionParserMiddleware.php
+++ Ephect/Modules/DataAccess/Middlewares/ConnectionParserMiddleware.php
@@ -2,6 +2,10 @@
 
 namespace Ephect\Modules\DataAccess\Middlewares;
 
+use Ephect\Framework\Event\EventDispatcher;
+use Ephect\Framework\Logger\Logger;
+use Ephect\Framework\Registry\StateRegistry;
+use Ephect\Modules\DataAccess\Events\ConnectionOpenerEvent;
 use Ephect\Modules\Forms\Components\ComponentEntityInterface;
 use Ephect\Modules\Forms\Middlewares\ComponentParserMiddlewareInterface;
 
@@ -18,6 +22,16 @@ class ConnectionParserMiddleware implements ComponentParserMiddlewareInterface
     ): void {
 //        StateRegistry::saveByMotherUid($motherUID, true);
         useState(["middlewares" => [ConnectionOpenerMiddleware::class => (object)$arguments],]);
+        $middleware = new ConnectionOpenerMiddleware();
+        $middleware((object) $arguments);
+
+        $eventsProvider = useState(get: 'eventsProvider');
+        // TODO: Fix event dispatcher issue
+//        Logger::create()->dump('eventsProvider', $eventsProvider);
+//        $connectionEvent = new ConnectionOpenerEvent((object) $arguments);
+//        $dispatcher = new EventDispatcher();
+//        $dispatcher->dispatch($connectionEvent);
+
 //        StateRegistry::save(true);
     }
 }
