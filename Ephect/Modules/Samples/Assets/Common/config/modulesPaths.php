f4521f8215d1ffd18234dcb67044f98197d2303b
David Blanchard <dpjb@hotmail.fr>
2025-10-24 19:26:44 +0200

    Fix modules configuration in samples assets

---
+++ Ephect/Modules/Samples/Assets/Common/config/modulesPaths.php
@@ -0,0 +1,13 @@
+<?php
+
+return [
+    \Constants::EPHECT_ROOT . 'Modules/DataAccess',
+    \Constants::EPHECT_ROOT . 'Modules/Forms',
+    \Constants::EPHECT_ROOT . 'Modules/Http',
+    \Constants::EPHECT_ROOT . 'Modules/JavaScripts',
+    \Constants::EPHECT_ROOT . 'Modules/Routing',
+    \Constants::EPHECT_ROOT . 'Modules/Samples',
+    \Constants::EPHECT_ROOT . 'Modules/WebApp',
+    \Constants::EPHECT_ROOT . 'Modules/WebComponent',
+]
+;
