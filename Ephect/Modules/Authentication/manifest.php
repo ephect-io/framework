01f32f25e5369c31d13df75bce4fbbf177d7a28f
David Blanchard <david.blanchard71@icloud.com>
2026-06-06 21:37:47 +0000

    fix: add missing manifest for authentication module

---
+++ Ephect/Modules/Authentication/manifest.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+    "tag" => "",
+    "name" => "ephect-io/data-access",
+    "entrypoint" => \Ephect\Modules\Forms\Components\Plugin::class,
+    "templates" => "!",
+    "description" => "Ephect framework authentication components.",
+    "version" => "0.7.2",
+];
