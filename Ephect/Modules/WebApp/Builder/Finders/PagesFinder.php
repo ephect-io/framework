084b37d1e26ce0cd8b3006c3450c80650d0bbb57
David Blanchard <david.blanchard71@icloud.com>
2026-06-13 10:31:26 +0000

    fix: try to take modules pages in account

--- Ephect/Modules/WebApp/Builder/Finders/PagesFinder.php
+++ Ephect/Modules/WebApp/Builder/Finders/PagesFinder.php
@@ -2,6 +2,7 @@
 
 namespace Ephect\Modules\WebApp\Builder\Finders;
 
+use Ephect\Framework\Modules\ModuleInstaller;
 use Ephect\Framework\Utils\File;
 
 class PagesFinder implements FinderInterface
@@ -13,6 +14,22 @@ class PagesFinder implements FinderInterface
         $pagesList = File::walkTreeFiltered(\Constants::CUSTOM_PAGES_ROOT, ['phtml']);
         foreach ($pagesList as $key => $pageFile) {
             $result[] = $pageFile;
+        }
+
+        [$filename, $modulePaths] = ModuleInstaller::readModulePaths();
+        foreach ($modulePaths as $path) {
+            $moduleSrcPath = ModuleInstaller::normalizeModuleSrcPath($path);
+            $pagePath = $moduleSrcPath . 'Forms' . DIRECTORY_SEPARATOR . 'Pages';
+            if(!is_dir($pagePath)) {
+                continue;
+            }
+            $pagesList = File::walkTreeFiltered($pagePath, ['phtml']);
+            if ($pagesList === false) {
+                continue;
+            }
+            foreach ($pagesList as $key => $pageFile) {
+                $result[] = $pageFile;
+            }
         }
         return $result;
     }
